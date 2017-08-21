fd8dee2947d2365d78975a994598cba5bef4854f
KieranWardle <kieran.wardle@ons.gov.uk>
2017-08-21 12:37:28 +0100

    CTPA-1508 Added ability to get resluts from select statements

--- cfdatabasetool/src/main/java/uk/gov/ons/ctp/cfdatabasetool/Endpoint.java
+++ cfdatabasetool/src/main/java/uk/gov/ons/ctp/cfdatabasetool/Endpoint.java
@@ -2,14 +2,18 @@ package uk.gov.ons.ctp.cfdatabasetool;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.net.URI;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
@@ -32,22 +36,36 @@ public final class Endpoint {
   private String passwd;
 
   @RequestMapping(method = RequestMethod.POST, consumes = "text/plain")
-  public void clearDatabase(final @Valid @RequestBody String sql) throws SQLException {
+  public ResponseEntity<String> runQuery(final @Valid @RequestBody String sql) throws SQLException {
 
     Properties props = new Properties();
     props.setProperty("user",uname);
     props.setProperty("password",passwd);
+
     Connection conn = DriverManager.getConnection(url, props);
     Statement stmt = null;
+    StringBuilder output =  new StringBuilder();
+
     try {
       stmt = conn.createStatement();
-      stmt.executeQuery(sql);
+      ResultSet rs = stmt.executeQuery(sql);
+      int columns = rs.getMetaData().getColumnCount();
+      for(int i = 1;i<columns;i++) {
+        output.append(rs.getMetaData().getColumnName(i)).append(",");
+      }
+      output.append(rs.getMetaData().getColumnName(columns)).append("\n");
+      while (rs.next()) {
+        for(int i = 1;i<columns;i++) {
+          output.append(rs.getArray(i)).append(",");
+        }
+        output.append(rs.getArray(columns)).append("\n");
+      }
     } catch (SQLException e) {
       System.out.println(e);
     } finally {
       if (stmt != null) { stmt.close(); }
     }
-
+    return ResponseEntity.created(URI.create("TODO")).body(output.toString());
   }
 
 }
