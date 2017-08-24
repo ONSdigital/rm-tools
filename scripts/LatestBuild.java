0ebc61704678f22d10467192b1093e52cc30872b
KieranWardle <kieran.wardle@ons.gov.uk>
2017-08-24 12:15:43 +0100

    Changed jar download to use git sha instead of just the latest

--- scripts/LatestBuild.java
+++ scripts/LatestBuild.java
@@ -20,37 +20,48 @@ public class LatestBuild {
 
   public static void main(String[] args) {
 
-    if(args.length != 4) {
+    String ext = "";
+    String optn = "";
+    String host = "";
+    String home = "";
+    String service = "";
+
+    if (args.length == 4 || args.length == 5) {
+      optn = args[0];
+      host = args[1];
+      home = args[2] + "/";
+      service = args[3];
+      if (args.length == 5 && optn.equals("-g")) {
+        String sha = args[4];
+        ext = ".git.sha" + sha;
+      } else if (args.length == 4 && optn.equals("-n")) {
+          if (service.equals(SURVEYSVC)) {
+            ext = ".tar";
+          }
+          else ext = ".jar";
+      } else {
+        System.err.println(USAGE);
+        System.exit(1);
+      }
+    } else {
       System.err.println(USAGE);
       System.exit(1);
     }
 
-    String optn = args[0];
-    String host = args[1];
-    String home = args[2] + "/";
-    String service = args[3];
-    String ext;
-    if (!(optn.equals("-g")) && !(optn.equals("-n"))) {
-      System.err.println(USAGE);
-      System.exit(1);
-    }
-    if(!Arrays.asList(SERVICES).contains(service)) {
+    if (!Arrays.asList(SERVICES).contains(service)) {
       System.err.println("Error! Service <" + service + "> does not exist!");
       System.exit(1);
     }
-    if (service.equals(SURVEYSVC)) {
-      ext = ".tar";
-    }
-    else ext = ".jar";
+
     String url = "" + host + "/artifactory/api/search/artifact?name=" +
-      service + "*" + ext + "&repos=libs-snapshot-local";
+       service + "*" + ext + "&repos=libs-snapshot-local";
 
     try {
       String snapshotList = getList(url, service, ext, home);
       String snapshotUrl = getSnapshotUrl(snapshotList, service);
-      if(optn.equals("-g")) {
-        getSvc(snapshotUrl, service, ext, home);
-      } else if(optn.equals("-n")) {
+      if (optn.equals("-g")) {
+        getSvc(snapshotUrl, home);
+      } else if (optn.equals("-n")) {
         getName(snapshotUrl, home, service);
       }
     } catch(Exception e) {
@@ -61,10 +72,7 @@ public class LatestBuild {
   private static String getList(String uri, String svc, String ext, String home)
       throws IOException {
 
-    URL url = new URL(uri);
-    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-    connection.setRequestMethod("GET");
-    connection.setRequestProperty("User-Agent", USER_AGENT);
+    HttpURLConnection connection = getConnection(uri);
 
     BufferedReader br = new BufferedReader(
       new InputStreamReader(connection.getInputStream()));
@@ -76,6 +84,7 @@ public class LatestBuild {
     }
 
     br.close();
+    connection.disconnect();
     return response.toString();
   }
 
@@ -89,24 +98,24 @@ public class LatestBuild {
       .split("\\,uri\\:")));
     int urls = snapshots.size();
     String url = "";
-    if(service.equals(SURVEYSVC)) {
+    if (service.equals(SURVEYSVC)) {
       snapshotList = snapshotList.replaceAll("[a-zA-Z\\.\\/\\-\\:]*", "");
       List<String> surveyVersions = new ArrayList<String>(Arrays
         .asList(snapshotList.split("\\,")));
 
       List<Integer> versionsNo = new ArrayList<Integer>();
-      for(String s : surveyVersions) {
+      for (String s : surveyVersions) {
         versionsNo.add(Integer.parseInt(s));
       }
       Collections.sort(versionsNo);
       int i = 0 ;
-      for(Integer v : versionsNo) {
+      for (Integer v : versionsNo) {
         surveyVersions.set(i, v.toString());
         i++;
       }
       i=0;
-      for(String snapshot : snapshots) {
-        if(snapshot.contains(surveyVersions.get(surveyVersions.size()-1))) {
+      for (String snapshot : snapshots) {
+        if (snapshot.contains(surveyVersions.get(surveyVersions.size()-1))) {
           url = snapshot;
         }
         i++;
@@ -126,22 +135,30 @@ public class LatestBuild {
     System.out.println(svcVersion);
   }
 
-  private static void getSvc(String uri, String svc, String ext, String home)
+  private static void getSvc(String uri, String home)
       throws IOException {
 
+    String url = uri.replaceAll("\\.git\\.sha.*$", ".jar");
+    System.out.println(url);
+    HttpURLConnection connection = getConnection(url);
+
+    BufferedReader br = new BufferedReader(
+      new InputStreamReader(connection.getInputStream()));
+    String[] urlComponents = url.split("/");
+    String filePath = home + urlComponents[12];
+    Path path = Paths.get(filePath);
+    Files.copy(connection.getInputStream(), path);
+    br.close();
+    connection.disconnect();
+  }
+
+  private static HttpURLConnection getConnection(String uri)
+      throws MalformedURLException, IOException, ProtocolException {
     URL url = new URL(uri);
     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
     connection.setRequestMethod("GET");
     connection.setRequestProperty("User-Agent", USER_AGENT);
 
-    int responseCode = connection.getResponseCode();
-
-    BufferedReader br = new BufferedReader(
-      new InputStreamReader(connection.getInputStream()));
-
-    String filePath = home + svc + ext;
-    Path path = Paths.get(filePath);
-    Files.copy(connection.getInputStream(), path);
-    br.close();
+    return connection;
   }
 }
