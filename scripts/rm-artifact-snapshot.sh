08aa2112b5c3b6a9b8939d1d6211e28ddc3d53b1
Simon <sdiaz1000@gmail.com>
2017-09-01 11:16:09 +0100

    CPTA-1514 Print out env details

--- scripts/rm-artifact-snapshot.sh
+++ scripts/rm-artifact-snapshot.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
  
+ echo WORKSPACE=$WORKSPACE
+ echo RM_PROJECT_GIT_NAME=$RM_PROJECT_GIT_NAME
+ echo RM_PROJECT_GIT_SHA=$RM_PROJECT_GIT_SHA
+ 
  set -e
  cd $WORKSPACE
  # Build and deploy SNAPSHOT
@@ -10,6 +14,10 @@
  SNAPSHOT_VERSION=`$MAVEN_HOME/mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep "^[^\[]"`
  GROUP_ID=`$MAVEN_HOME/mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.groupId | grep "^[^\[]"`
  ARTIFACT_ID=`$MAVEN_HOME/mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.artifactId | grep "^[^\[]"`
+
+ echo SNAPSHOT_VERSION=$SNAPSHOT_VERSION
+ echo GROUP_ID=$GROUP_ID
+ echo ARTIFACT_ID=$ARTIFACT_ID
 
  # Get name of latest build in artifactory and build and deploy sha file
  TIMESTAMP=$(curl "http://artifactory.rmdev.onsdigital.uk/artifactory/libs-snapshot-local/uk/gov/ons/ctp/product/$ARTIFACT_ID/$SNAPSHOT_VERSION/maven-metadata.xml" | \
