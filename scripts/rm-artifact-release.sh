ded717019b3a9d6217dc16aa25785c3f54412021
Kieran Wardle <kieran.wardle@ons.gov.uk>
2017-09-11 13:01:10 +0100

    Update rm-artifact-release.sh

--- scripts/rm-artifact-release.sh
+++ scripts/rm-artifact-release.sh
@@ -23,29 +23,7 @@ echo GROUP_ID=$GROUP_ID
 echo ARTIFACT_ID=$ARTIFACT_ID
 echo RELEASE_FILENAME=$RELEASE_FILENAME
 
-
-# Get Snapshot based on sha
-#VERSION=$(curl http://artifactory.rmdev.onsdigital.uk/artifactory/libs-snapshot-local/uk/gov/ons/ctp/product/$ARTIFACT_ID/maven-metadata.xml | \
-#    awk '/<latest>/' | \
-#    sed 's/.*<latest>\([0-9\.]*\)\-SNAPSHOT<.*/\1/')
-#TIMESTAMP=$(curl http://artifactory.rmdev.onsdigital.uk/artifactory/api/search/artifact?name=$ARTIFACT_ID*$RM_PROJECT_GIT_SHA | echo$(grep $RM_PROJECT_GIT_SHA) | \
-#    sed "s/.*$ARTIFACT_ID\-\(.*\)\.git.*/\1/")
-#..JAR
-#curl  http://artifactory.rmdev.onsdigital.uk/artifactory/libs-snapshot-local/uk/gov/ons/ctp/product/$ARTIFACT_ID/$VERSION\-SNAPSHOT/$ARTIFACT_ID-$TIMESTAMP.jar > $ARTIFACT_ID-$TIMESTAMP.jar 
-#mv *.jar $WORKSPACE/$RELEASE_FILENAME.jar
-#..Manifest
-#curl  http://artifactory.rmdev.onsdigital.uk/artifactory/libs-snapshot-local/uk/gov/ons/ctp/product/$ARTIFACT_ID/$VERSION\-SNAPSHOT/$ARTIFACT_ID-$TIMESTAMP.manifest-template.yml > $ARTIFACT_ID-$TIMESTAMP.manifest-template.yml 
-#mv $ARTIFACT_ID-$TIMESTAMP.manifest-template.yml $WORKSPACE/manifest-template-$RELEASE_VERSION.yml
-
-
 # Deploy Release to artifactory
-#cd $WORKSPACE/
-#export GROUP_PATH=$(echo $GROUP_ID | tr '.' '/')
-#curl -u build:$ARTIFACTORY_PASSWORD -X PUT "http://artifactory.rmdev.onsdigital.uk/artifactory/libs-release-local/$GROUP_PATH/$ARTIFACT_ID/$RELEASE_VERSION/$RELEASE_FILENAME.jar" -T $RELEASE_FILENAME.jar
-#if [ $? -ne 0 ]; then exit 1;  fi
-#curl -u build:$ARTIFACTORY_PASSWORD -X PUT "http://artifactory.rmdev.onsdigital.uk/artifactory/libs-release-local/$GROUP_PATH/$ARTIFACT_ID/$RELEASE_VERSION/$RELEASE_FILENAME.pom" -T $RELEASE_FILENAME.pom
-#if [ $? -ne 0 ]; then exit 1;  fi
-
 $MAVEN_HOME/mvn clean deploy
 curl -u build:$ARTIFACTORY_PASSWORD -X PUT "http://artifactory.rmdev.onsdigital.uk/artifactory/libs-release-local/$GROUP_PATH/$ARTIFACT_ID/$RELEASE_VERSION/manifest-template-$RELEASE_VERSION.yml" -T manifest-template.yml
 if [ $? -ne 0 ]; then exit 1;  fi
