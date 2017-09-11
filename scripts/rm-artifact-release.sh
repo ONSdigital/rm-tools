b1213b2b61a0106ea796b4513001279d96146c69
Kieran Wardle <kieran.wardle@ons.gov.uk>
2017-09-11 11:50:56 +0100

    Update rm-artifact-release.sh

--- scripts/rm-artifact-release.sh
+++ scripts/rm-artifact-release.sh
@@ -8,9 +8,9 @@ set -e
 cd $WORKSPACE/
 git clone git@github.com:ONSdigital/$RM_PROJECT_GIT_NAME.git
 git reset --hard $RM_PROJECT_GIT_SHA
-git push origin $RM_PROJECT_GIT_SHA
 RELEASE_VERSION=`$MAVEN_HOME/mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep "^[^\[]"`
 git checkout -b $RELEASE_VERSION
+git push origin $RM_PROJECT_GIT_SHA
 mvn dependency:tree | awk '/uk.gov.ons.ctp.product.*SNAPSHOT:compile/{err = 1} END {exit err}'
 $MAVEN_HOME/mvn versions:set -DremoveSnapshot=true
 GROUP_ID=`$MAVEN_HOME/mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.groupId | grep "^[^\[]"`
