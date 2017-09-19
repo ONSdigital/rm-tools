8a903b25342a8fe68ba90377593feaa5193373e1
KieranWardle <kieran.wardle@ons.gov.uk>
2017-09-19 12:07:06 +0100

    Added script to check if all cucumber tests passed

---
+++ scripts/buildSuccessCheck.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+TOKEN=$1
+JENKINS=$2
+JOBNAME=CTP_Cucumber_16_Offline_Response
+BUILD_NO=lastBuild
+declare -i I=0
+while [ $I -lt 16 ]
+do
+  echo JOBNAME: $JOBNAME
+  echo BUILD_NO: $BUILD_NO
+  STATUS=$(curl "http://$TOKEN@$JENKINS/job/$JOBNAME/$BUILD_NO/api/json" | grep \"result\"\:\"SUCCESS\")
+  if [ $? -eq 1 ]
+  then
+    echo "FAILURE on $JOBNAME : $BUILD_NO"
+    exit 1
+  fi
+  JOBNAME=$(echo $STATUS | sed 's/.*Started by upstream project \\"\(CTP\_Cucumber\_[0-9]\{2\}_[a-zA-Z\_]*\)\\".*/\1/' )
+  BUILD_NO=$(echo $STATUS | sed 's/.*\"upstreamBuild\"\:\([0-9]*\).*/\1/')
+  I=$I+1
+done
+echo "All tests passed!"
+
+mkdir SUCCESSFUL_CONFIG_$TS
+
+git clone git@github.com:ONSdigital/sdc-service-versions.git
+cd sdc-service-versions
+git checkout ci
+
+cp services/* ../SUCCESSFUL_CONFIG_$TS
+
+git checkout ci-successful-config
+cp ../SUCCESSFUL_CONFIG_$TS .
+git commit -am "Full test success"
