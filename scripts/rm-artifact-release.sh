e406ad94a179deacfb10dcf6eabb792fc2ca25e0
Kieran Wardle <kieran.wardle@ons.gov.uk>
2017-10-19 13:18:42 +0100

    Update rm-artifact-release.sh

--- scripts/rm-artifact-release.sh
+++ scripts/rm-artifact-release.sh
@@ -15,7 +15,7 @@ $MAVEN_HOME/mvn versions:set -DremoveSnapshot=true
 $MAVEN_HOME/mvn dependency:tree | awk '/uk.gov.ons.ctp.product.*SNAPSHOT:compile/{err = 1} END {exit err}'
 RELEASE_VERSION=`$MAVEN_HOME/mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.version | grep "^[0-9\.]\{7,9\}"`
 git checkout -b $RELEASE_VERSION
-#git push origin $RELEASE_VERSION
+git push origin $RELEASE_VERSION
 GROUP_ID=`$MAVEN_HOME/mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.groupId | grep "^uk\.gov\.ons\.ctp\.[a-z]*$"`
 ARTIFACT_ID=`$MAVEN_HOME/mvn org.apache.maven.plugins:maven-help-plugin:2.2:evaluate -Dexpression=project.artifactId | grep "^[a-z\-]*$"`
 RELEASE_FILENAME=$ARTIFACT_ID-$RELEASE_VERSION
