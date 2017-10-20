3c2552e4d1fdfa7beb89cd4003b203731af06e25
Kieran Wardle <kieran.wardle@ons.gov.uk>
2017-10-20 15:32:17 +0100

    Update cf_deployment_jobs.sh

--- scripts/pipeline/cf_deployment_jobs.sh
+++ scripts/pipeline/cf_deployment_jobs.sh
@@ -8,12 +8,12 @@ echo $VERSION | grep "\-SNAPSHOT"
 if [ $? -eq 1 ]
 then
   echo "Deploying RELEASE $VERSION to $SPACE"
-  curl "$RELEASE_URL/$SERVICE/$VERSION/$SERVICE-$VERSION.$EXT" > target/${SERVICE}.$EXT
+  curl "${RELEASE_URL}/$SERVICE/$VERSION/$SERVICE-$VERSION.$EXT" > target/${SERVICE}.$EXT
 else
   echo SHA=$SHA
   echo "Deploying SNAPSHOT $VERSION to $SPACE"
   TIMESTAMP=$(curl ${ARTIFACTORY_URL}/artifactory/api/search/artifact?name=$SERVICE* | grep -m 1 ".*$SHA[0-9a-z]\{33\}" | \
     sed "s/.*$SERVICE\-\(.*\)\.git.*/\1/")
   echo TIMESTAMP=$TIMESTAMP
-  curl "$SNAPSHOT_URL/$SERVICE/$VERSION-SNAPSHOT/$SERVICE-$TIMESTAMP.$EXT" > target/${SERVICE}.$EXT
+  curl "${SNAPSHOT_URL}/$SERVICE/$VERSION-SNAPSHOT/$SERVICE-$TIMESTAMP.$EXT" > target/${SERVICE}.$EXT
 fi
