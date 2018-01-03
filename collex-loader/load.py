a7a0e9dd4fb878db0b1a25addba3a5e965657a77
Matt Innes <matt@madeye.com>
2018-01-03 10:28:07 +0000

    Minimal version

--- collex-loader/load.py
+++ collex-loader/load.py
@@ -2,6 +2,12 @@ import csv
 import json
 
 with open('CE_Details_for_loading_into_RM.csv') as fp:
-	reader = csv.DictReader(fp)
-	with open('CE_Details_for_loading_into_RM.json', 'w') as fo:
-		json.dump([row for row in reader], fo)
+    reader = csv.DictReader(fp)
+    print (reader)
+    for row in reader:
+        survey_id = row['Survey_Id']
+        period = row['Collection_Period']
+        if survey_id and period:
+            filename = "%s-%s.json" % (survey_id, period)
+            with open(filename, 'w') as fo:
+                json.dump(row, fo)
