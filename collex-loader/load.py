0fe7677412e25a6500ac8d324ae3ae73225a5f5d
Matt Innes <matt@madeye.com>
2018-01-03 15:54:39 +0000

    Loader is now complete
    It will accept a CSV from the business, map the data into requests to
    the collection exercise service and print the results.  It also supports
    a dry run mode where it will write the requests out as files instead of
    making HTTP POST requests.

--- collex-loader/load.py
+++ collex-loader/load.py
@@ -1,13 +1,59 @@
 import csv
 import json
+import argparse
+import requests
+from functools import partial
 
-with open('CE_Details_for_loading_into_RM.csv') as fp:
-    reader = csv.DictReader(fp)
-    print (reader)
-    for row in reader:
-        survey_id = row['Survey_Id']
-        period = row['Collection_Period']
-        if survey_id and period:
-            filename = "%s-%s.json" % (survey_id, period)
-            with open(filename, 'w') as fo:
-                json.dump(row, fo)
+def parse_args():
+    parser = argparse.ArgumentParser(description='Load collection exercise CSV.')
+    parser.add_argument("config", help="Configuration file")
+    return parser.parse_args() 
+
+def map_columns(column_mappings, row):
+    new_row = dict()
+    for key, value in row.items():
+        if key and value:
+            new_row[column_mappings[key] if column_mappings[key] else key] = value
+    return new_row
+
+def post_collex(data, url, user, password):
+    response = requests.post(url, json=data, auth=(user, password))
+
+    status_code = response.status_code
+    detail_text = response.text if status_code != 201 else ''
+
+    print("%s <= %s (%s)" % (status_code, data, detail_text))
+
+def dump_collex(data):
+    survey_id = data['surveyRef']
+    period = data['exerciseRef']
+    if survey_id and period:
+        filename = "%s-%s.json" % (survey_id, period)
+        with open(filename, 'w') as fo:
+            json.dump(data, fo)
+
+def clean_row(row):
+    row['name'] = row['name'][:20]
+
+    return row
+        
+def process_file(filename, row_handler):
+    with open(filename) as fp:
+        reader = csv.DictReader(fp)
+        for row in reader:
+            new_row = map_columns(column_mappings, row)
+
+            if new_row:
+                row_handler(data=clean_row(new_row))
+
+args = parse_args()
+print("Config filename: %s" % args.config)
+config = json.load(open(args.config))
+input_file = config['inputFile']
+print("Input filename: %s" % input_file)
+column_mappings = config['columnMappings']
+api_url = config['api']['url']
+print ("API URL: %s" % api_url)
+row_handler = dump_collex if config['dryRun'] else partial(post_collex, url=api_url, user=config['api']['user'], password=config['api']['password'])
+
+result = process_file(input_file, row_handler)
