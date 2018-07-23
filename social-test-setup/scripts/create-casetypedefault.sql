efe06c348973941f64e553796482875d11afc9de
AdamHawtin <adam.hawtin@ons.gov.uk>
2018-07-23 10:37:45 +0100

    Remove unused SQL file

--- social-test-setup/scripts/create-casetypedefault.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO collectionexercise.casetypedefault
-(casetypedefaultpk, sampleunittypefk, actionplanid, survey_uuid)
-VALUES(4, 'H', '3a4be6b6-4d42-411d-8f05-1e6bba678844', 'eb63b22f-2ac5-4262-b3f1-744c8d96ddfb');
